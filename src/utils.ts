8cc5e011f7c91b5e6d4c00cb123827ebac6e8786
Robert Kuykendall <robert@rkuykendall.com>
2019-02-20 17:09:27 -0500

    More stable and tested percentage functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-import { DATE_FORMATS } from './constants';
+import Decimal from 'decimal.js';
+
+import { CENT_DECIMAL, DATE_FORMATS } from './constants';
 import { formatDate } from './formatting';
 
 export function insertIf (condition: boolean, element: any): any[] {
@@ -7,4 +9,16 @@ export function insertIf (condition: boolean, element: any): any[] {
 
 export function dateToday () {
   return formatDate((new Date()).toISOString(), DATE_FORMATS.date_value);
+}
+
+export function getPercentValue (value?: null | string): string {
+  if (typeof value === 'undefined' || value === null || value === '') { return ''; }
+  const decimal = new Decimal(value).div(CENT_DECIMAL);
+  return decimal.toString();
+}
+
+export function getPercentDisplay (value?: null | string): string {
+  if (typeof value === 'undefined' || value === null || value === '') { return ''; }
+  const decimal = new Decimal(value).times(CENT_DECIMAL);
+  return decimal.toString();
 }
