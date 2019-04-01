7038734f029a13301c7ade249d1775e7d74ae6e6
Keven Bouchard <peckgfx@gmail.com>
2019-04-01 12:56:10 -0400

    v1.1.2

--- dist/containers.d.ts
+++ dist/containers.d.ts
@@ -3,9 +3,9 @@ export interface IDisabledContainerProps {
     className?: any;
 }
 export interface IGuardedContainerProps {
-    disabledComponent: React.ComponentClass;
-    enabledComponent: React.ComponentClass;
+    disabledComponent: React.ReactNode;
+    enabledComponent: React.ReactNode;
     isGuarded: boolean;
 }
-export declare function createDisabledContainer(WrappedComponent: React.ComponentClass<any>): React.ComponentClass;
+export declare function createDisabledContainer(WrappedComponent: React.ComponentType<any>): React.ReactNode;
 export declare function createGuardedContainer({ isGuarded, enabledComponent, disabledComponent }: IGuardedContainerProps): React.ComponentClass;
