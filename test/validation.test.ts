bda7c13677f3c40ebf96c59e39744d2671c189ba
Robert Kuykendall <robert@rkuykendall.com>
2019-04-08 15:22:29 -0400

    Add date utils for Birthdate

---
+++ test/validation.test.ts
@@ -0,0 +1,21 @@
+import * as util from '../src';
+
+describe('validation', () => {
+  it('isValidBirthdate', () => {
+    expect(util.isValidBirthdate('8-01-01')).toBe(false);
+    expect(util.isValidBirthdate('98-01-01')).toBe(false);
+    expect(util.isValidBirthdate('998-01-01')).toBe(false);
+    expect(util.isValidBirthdate('1998-1-1')).toBe(false);
+
+    expect(util.isValidBirthdate('0000-00-00')).toBe(false);
+    expect(util.isValidBirthdate('0000-00-01')).toBe(false);
+    expect(util.isValidBirthdate('0000-01-00')).toBe(false);
+    expect(util.isValidBirthdate('0000-01-01')).toBe(true);
+
+    expect(util.isValidBirthdate('0998-01-01')).toBe(true);
+    expect(util.isValidBirthdate('1989-11-22')).toBe(true);
+    expect(util.isValidBirthdate('1998-01-01')).toBe(true);
+    expect(util.isValidBirthdate('2020-01-01')).toBe(false);
+  });
+});
+
