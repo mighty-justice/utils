0433dc08d378b42aaa40894b3028d6bf1530f17c
Robert Kuykendall <robert@rkuykendall.com>
2019-04-12 12:47:00 -0400

    Self-CR

--- src/validation.ts
+++ src/validation.ts
@@ -2,8 +2,7 @@ import moment from 'moment';
 
 export function isValidBirthdate (value: string) {
   return !value || (
-      !!value // Not empty
-      && value.length === '####-##-##'.length // ISO date
+      value.length === '####-##-##'.length // ISO date
       && moment(value).isValid() // Real day
       && moment(value).isBefore(moment()) // In the past
     );
