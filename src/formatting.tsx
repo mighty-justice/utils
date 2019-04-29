0239354fe2695147abda32aacf49b210980a1b4e
Robert Kuykendall <robert@rkuykendall.com>
2019-04-29 16:56:05 -0400

    Add testing for varToLabel and support VARS LIKE THIS

--- src/formatting.tsx
+++ src/formatting.tsx
@@ -249,18 +249,29 @@ export function getDisplayName (component: any): (string | undefined) {
 function _varToLabel (str: string) {
   // Sourced significantly from https://github.com/gouch/to-title-case/blob/master/to-title-case.js
   const smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i
-    , suffix = (str).split('.').pop() || ''
-    , formatted = startCase(suffix);
+    , suffix = str.split('.').pop() || ''
+    , formatted = startCase(suffix)
+    ;
 
   return formatted.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, (match: string, index: number, title: string) => {
-    if (index > 0 && index + match.length !== title.length &&
-      match.search(smallWords) > -1 && title.charAt(index - 2) !== ':' &&
-      (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-      title.charAt(index - 1).search(/[^\s-]/) < 0) {
+    const notFirstWord = index > 0
+      , notOnlyWord = index + match.length !== title.length
+      , hasSmallWords = match.search(smallWords) > -1
+      , notAfterColon = title.charAt(index - 2) !== ':'
+      , notFirstPartOfHyphen = (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-')
+      ;
+
+    if (
+      notFirstWord
+      && notOnlyWord
+      && hasSmallWords
+      && notAfterColon
+      && notFirstPartOfHyphen
+    ) {
       return match.toLowerCase();
     }
 
-    return match.charAt(0).toUpperCase() + match.substr(1);
+    return match.charAt(0).toUpperCase() + match.substr(1).toLowerCase();
   });
 }
 
