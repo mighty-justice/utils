b48b133be3e838f62c3d19acbc5e273b1fcf7fd5
Robert Kuykendall <robert@rkuykendall.com>
2019-05-14 13:27:41 -0400

    formatAddressMultiline should output 2 lines not 3

--- src/formatting.tsx
+++ src/formatting.tsx
@@ -302,5 +302,5 @@ export function formatAddress (address?: IAddress | null) {
 }
 
 export function formatAddressMultiline (address?: IAddress | null) {
-  return parser(formatAddress(address).replace(/, /g, '<br/>'));
+  return parser(formatAddress(address).replace(/, /, '<br/>'));
 }
