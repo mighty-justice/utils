544cbe24e97beffeae4c11dbcb1234e066964161
Robert Kuykendall <robert@rkuykendall.com>
2021-09-20 15:05:28 -0500

    CR renname

--- src/utils.ts
+++ src/utils.ts
@@ -7,19 +7,19 @@ export function insertIf<T>(condition: boolean, element: T): T[] {
   return condition ? [element] : [];
 }
 
-function _isDecimalValue(value: unknown): value is Decimal.Value {
+function _convertibleToDecimalObject(value: unknown): value is Decimal.Value {
   return (isString(value) && value !== '') || isNumber(value) || Decimal.isDecimal(value);
 }
 
 export function getPercentValue(value?: unknown): string {
-  if (!_isDecimalValue(value)) {
+  if (!_convertibleToDecimalObject(value)) {
     return '';
   }
   return new Decimal(value).div(CENT_DECIMAL).toString();
 }
 
 export function getPercentDisplay(value?: unknown): string {
-  if (!_isDecimalValue(value)) {
+  if (!_convertibleToDecimalObject(value)) {
     return '';
   }
   return new Decimal(value).times(CENT_DECIMAL).toString();
